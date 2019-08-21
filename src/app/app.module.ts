6747b9e19649c515350f62e13ea7a2ede107f38c
Benjamin Mathieu <mathieu.b93@gmail.com>
2019-08-21 10:51:22 +0200

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
+import { SocialComponent } from './social/social.component';
+import { VitaComponent } from './vita/vita.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    SocialComponent,
+    VitaComponent
   ],
   imports: [
     BrowserModule
