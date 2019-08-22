3680fcb23ffb47721cd29725134499584e41e0fc
Benjamin Mathieu <mathieu.b93@gmail.com>
2019-08-22 10:53:50 +0200

    design forrest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
 import { SocialComponent } from './social/social.component';
 import { VitaComponent } from './vita/vita.component';
-import { WavesComponent } from './waves/waves.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonComponent,
     SocialComponent,
-    VitaComponent,
-    WavesComponent
+    VitaComponent
   ],
   imports: [
     BrowserModule
