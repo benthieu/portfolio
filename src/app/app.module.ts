342db598fc42e8dadfb6604d023db19f534a4edc
Benjamin Mathieu <mathieu.b93@gmail.com>
2019-08-23 15:43:31 +0200

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
 import { SocialComponent } from './social/social.component';
-import { VitaComponent } from './vita/vita.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonComponent,
-    SocialComponent,
-    VitaComponent
+    SocialComponent
   ],
   imports: [
     BrowserModule
